f6561a3272db29c5ddb9b4ed8ba1c92462f62cf7
“Jen <Jen.j.eyring@gmail.com>
2022-06-01 19:37:28 -0500

    redoing it

--- wrangle.py
+++ wrangle.py
@@ -1,36 +1,78 @@
 import pandas as pd
-import env
-from babel.numbers import format_currency
+import os
+from env import get_db_url
+
+"""
+USAGE: 
+Use `from wrangle import wrangle_zillow` at the top of your notebook.
+This 
+"""
+def get_zillow_data():
+    """Seeks to read the cached zillow.csv first """
+    filename = "zillow.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        return get_new_zillow_data()
+
+def get_new_zillow_data():
+    """Returns a dataframe of all 2017 properties that are Single Family Residential"""
+
+    sql = """
+    select 
+    bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+    from properties_2017
+    join propertylandusetype using (propertylandusetypeid)
+    where propertylandusedesc = "Single Family Residential"
+    """
+    return pd.read_sql(sql, get_db_url("zillow"))
 
 
-
-
-def wrangle_zillow(df):
-    '''Read the zillow sql database into a pandas dataframe 
-        where only the 2017 Single Family residential properties show along with the following columns:
-        bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, 
-        taxvaluedollarcnt, yearbuilt, taxamount, and fips.
-
-        Dropping all nulls from the dataset,
-        and converting dtypes to drop decimals on fips, yearbuilt, and calculated square feet columns.
-
-        Imported currency converter for taxamount and taxvalue in USD.
-
-        Rename columns to: Bedroom_count, Bathroom_count, square_feet, Tax_value, Tax_amount and Fips_code
-    '''
-
-    #Acquire zillow database from MySQL
-    df = pd.read_sql("SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt,  taxamount , fips FROM properties_2017 JOIN propertylandusetype USING (propertylandusetypeid)WHERE propertylandusetype.propertylandusedesc = 'Single Family Residential';", env.get_db_url('zillow'))
-
-    #Dropped all nulls
+def handle_nulls(df):    
+    # We keep 99.41% of the data after dropping nulls
+    # round(df.dropna().shape[0] / df.shape[0], 4) returned .9941
     df = df.dropna()
+    return df
 
-    #Changing dtype of Fips, squarefeet and yearbuilt
-    df['fips'] = df['fips'].astype('int')
-    df['yearbuilt'] = df['yearbuilt'].astype('int')
-    df['calculatedfinishedsquarefeet'] = df['calculatedfinishedsquarefeet'].astype('int')
 
-    #Renamed columns for easier reading
-    df = df.rename(columns={"bedroomcnt": "Bedroom_Count", "bathroomcnt": "Bathroom_count", "calculatedfinishedsquarefeet": "square_feet", "taxvaluedollarcnt": "Tax_Value", "taxamount": "Tax_amount", "fips": "Fips_code"})
+def optimize_types(df):
+    # Convert some columns to integers
+    # fips, yearbuilt, and bedrooms can be integers
+    df["fips"] = df["fips"].astype(int)
+    df["yearbuilt"] = df["yearbuilt"].astype(int)
+    df["bedroomcnt"] = df["bedroomcnt"].astype(int)    
+    df["taxvaluedollarcnt"] = df["taxvaluedollarcnt"].astype(int)
+    df["calculatedfinishedsquarefeet"] = df["calculatedfinishedsquarefeet"].astype(int)
+    return df
+
+
+def handle_outliers(df):
+    """Manually handle outliers that do not represent properties likely for 99% of buyers and zillow visitors"""
+    df = df[df.bathroomcnt <= 6]
+    
+    df = df[df.bedroomcnt <= 6]
+
+    df = df[df.taxvaluedollarcnt < 2_000_000]
+
+    return df
+
+def wrangle_zillow():
+    """
+    Acquires Zillow data
+    Handles nulls
+    optimizes or fixes data types
+    handles outliers w/ manual logic
+    returns a clean dataframe
+    """
+    df = get_zillow_data()
+
+    df = handle_nulls(df)
+
+    df = optimize_types(df)
+
+    df = handle_outliers(df)
+
+    df.to_csv("zillow.csv", index=False)
 
     return df
