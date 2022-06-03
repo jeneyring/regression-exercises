3f1abbecb832c022f37b215e759ad7471c540249
“Jen <Jen.j.eyring@gmail.com>
2022-06-03 14:32:41 -0500

    keeping on

--- evaluate.py
+++ evaluate.py
@@ -37,14 +37,14 @@ def plot_residuals(df, y, yhat):
     # residual plots (x vs residual)
     plt.figure(figsize = (11,5))
     plt.subplot(121)
-    plt.scatter(df.x, df.baseline_residual)
+    plt.scatter(df.y, df.baseline_residual)
     plt.axhline(y = 0, ls = ':')
     plt.xlabel('x')
     plt.ylabel('Residual')
     plt.title('Baseline Residuals')
 
     plt.subplot(122)
-    plt.scatter(df.x, df.residual)
+    plt.scatter(df.y, df.residual)
     plt.axhline(y = 0, ls = ':')
     plt.xlabel('x')
     plt.ylabel('Residual')
@@ -53,7 +53,7 @@ def plot_residuals(df, y, yhat):
 
 #_______________________________________________
 
-def regression_errors(df, y, yhat):
+def regression_errors(y, yhat):
     """
     A function that takes in a dataset and creates a regression_errors(y, yhat): 
     and returns the following values:
@@ -64,37 +64,70 @@ def regression_errors(df, y, yhat):
     - root mean squared error (RMSE)"""
 #first calculate the square of residuals (makes it all positive)
     df['residual^2'] = df.residual**2
-    df['baseline_residual^2'] = df.baseline_residual**2
-#sum of squared errors (SSE)
     SSE = df['residual^2'].sum()
-    SSE_baseline = df['baseline_residual^2'].sum()
-#mean squared error (MSE)
     MSE = SSE/len(df)
+    TSS = SSE = df['residual^2'].sum()
+    ESS = TSS - SSE
+    RMSE = sqrt(MSE)
+    
+    print("SSE =", "{:.1f}".format(SSE))
+    print("MSE = ", "{:.1f}".format(MSE))
+    print("TSS = ","{:.1f}".format(TSS))
+    print("ESS = ","{:.1f}".format(ESS))
+    print("RMSE = ", "{:.1f}".format(RMSE))
+
+#___________________________________________________________
+
+def baseline_mean_errors(y):
+    """
+    A function that takes in a dataset and creates a baseline_mean_errors(y): 
+    and returns the following values:
+    - sum of squared errors (SSE)
+    - mean squared error (MSE)
+    - root mean squared error (RMSE)
+    """
+#first calculate the baseline square of residuals (makes it all positive)
+    df['baseline_residual^2'] = df.baseline_residual**2
+
+#sum of squared errors (SSE)
+
+    SSE_baseline = df['baseline_residual^2'].sum()
+
+#mean squared error (MSE)
+
     MSE_baseline = SSE_baseline/len(df)
 
 #TOTAL SUM OF SQUARES
-    TSS = SSE = df['residual^2'].sum()
+
     TSS_baseline = SSE_baseline = df['baseline_residual^2'].sum()
 
 #EXPLAINED SUM of SQUARES
-    ESS = TSS - SSE
+
     ESS_baseline = TSS_baseline - SSE-baseline
 
 #ROOT MEAN SQUARED ERROR
-    RMSE = sqrt(MSE)
+
     RMSE_baseline =  sqrt(MSE_baseline)
 
-print('SSE =', "{:.1f}".format(SSE))
+
 print("SSE Baseline =", "{:.1f}".format(SSE_baseline))
 
-print("MSE = ", "{:.1f}".format(MSE))
+
 print("MSE baseline = ", "{:.1f}".format(MSE_baseline))
 
-print("TSS = ","{:.1f}".format(TSS)) 
-print("TSS_baseline = ","{:.1f}".format(TSS_baseline))
 
-print("ESS = ","{:.1f}".format(ESS))
-print("ESS_baseline = ","{:.1f}".format(ESS_baseline))
-
-print("RMSE = ", "{:.1f}".format(RMSE))
 print("RMSE baseline = ", "{:.1f}".format(RMSE_baseline))
+
+
+#_______________________________________________________
+
+def better_than_baseline(y, yhat):
+
+    Baseline = baseline_mean_errors['SSE_baseline','MSE_baseline','RMSE_baseline'].sum()
+
+    Model = regression_errors['SSE','MSE','RMSE'].sum()
+
+    if Baseline > Model: 
+        print ('Model beats baseline')
+    else:
+        print('Baseline beats Model')
