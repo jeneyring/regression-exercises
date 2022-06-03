c600c95413180f3edacaed7dfa139301277545e8
“Jen <Jen.j.eyring@gmail.com>
2022-06-03 12:24:15 -0500

    adding to the evaluation

---
+++ evaluate.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from math import sqrt
+
+from sklearn.model_selection import train_test_split
+from scipy.stats import pearsonr, spearmanr
+
+
+
+"""
+A py file that holds the evaluation codes needed for the zillow dataset.
+Coded functions include:
+- plot_residuals(y, yhat): creates a residual plot 
+- regression_errors(y, yhat): returns the following values:
+    - sum of squared errors (SSE)
+    - explained sum of squares (ESS)
+    - total sum of squares (TSS)
+    - mean squared error (MSE)
+    - root mean squared error (RMSE)
+- baseline_mean_errors(y): computes the SSE, MSE, and RMSE for the baseline model
+- better_than_baseline(y, yhat): returns true if your model performs better than the baseline, otherwise false
+"""
+
+def plot_residuals(df, y, yhat):
+    """
+    A function that takes in a dataset, y= df.'target_var', yhat
+    and return a residuals plot (baseline and model predictor)
+    """
+    #create a residual columns on df
+    df['residual'] = df.y - df.yhat
+    #create a baseline_residual
+    df['baseline_residual'] = df.y - df.baseline
+    
+    # residual plots (x vs residual)
+    plt.figure(figsize = (11,5))
+    plt.subplot(121)
+    plt.scatter(df.x, df.baseline_residual)
+    plt.axhline(y = 0, ls = ':')
+    plt.xlabel('x')
+    plt.ylabel('Residual')
+    plt.title('Baseline Residuals')
+
+    plt.subplot(122)
+    plt.scatter(df.x, df.residual)
+    plt.axhline(y = 0, ls = ':')
+    plt.xlabel('x')
+    plt.ylabel('Residual')
+    plt.title('OLS model residuals');
+    return plt.show()
+
+#_______________________________________________
+
+def regression_errors(df, y, yhat):
+    """
+    A function that takes in a dataset and creates a regression_errors(y, yhat): 
+    and returns the following values:
+    - sum of squared errors (SSE)
+    - explained sum of squares (ESS)
+    - total sum of squares (TSS)
+    - mean squared error (MSE)
+    - root mean squared error (RMSE)"""
+#first calculate the square of residuals (makes it all positive)
+    df['residual^2'] = df.residual**2
+    df['baseline_residual^2'] = df.baseline_residual**2
+#sum of squared errors (SSE)
+    SSE = df['residual^2'].sum()
+    SSE_baseline = df['baseline_residual^2'].sum()
+#mean squared error (MSE)
+    MSE = SSE/len(df)
+    MSE_baseline = SSE_baseline/len(df)
+
+#TOTAL SUM OF SQUARES
+    TSS = SSE = df['residual^2'].sum()
+    TSS_baseline = SSE_baseline = df['baseline_residual^2'].sum()
+
+#EXPLAINED SUM of SQUARES
+    ESS = TSS - SSE
+    ESS_baseline = TSS_baseline - SSE-baseline
+
+#ROOT MEAN SQUARED ERROR
+    RMSE = sqrt(MSE)
+    RMSE_baseline =  sqrt(MSE_baseline)
+
+print('SSE =', "{:.1f}".format(SSE))
+print("SSE Baseline =", "{:.1f}".format(SSE_baseline))
+
+print("MSE = ", "{:.1f}".format(MSE))
+print("MSE baseline = ", "{:.1f}".format(MSE_baseline))
+
+print("TSS = ","{:.1f}".format(TSS)) 
+print("TSS_baseline = ","{:.1f}".format(TSS_baseline))
+
+print("ESS = ","{:.1f}".format(ESS))
+print("ESS_baseline = ","{:.1f}".format(ESS_baseline))
+
+print("RMSE = ", "{:.1f}".format(RMSE))
+print("RMSE baseline = ", "{:.1f}".format(RMSE_baseline))
